a8ddcca1f3abaef81c43f99b27a7eb7d9a83dc0d
rahulkpatil <rahulkishor.patil@neosofttech.com>
2019-10-07 12:56:46 +0530

    Resolve employee repository issues for create employee

--- api/models/employee_model.go
+++ api/models/employee_model.go
@@ -22,7 +22,7 @@ type Employee struct {
 type Titles struct {
 	gorm.Model
 	EmpNo    int32  `gorm:"emp_no" json:"emp_no"`
-	title    string `gorm:"title" json:"title"`
+	Title    string `gorm:"title" json:"title"`
 	FromDate time.Time
 	ToDate   time.Time
 }
